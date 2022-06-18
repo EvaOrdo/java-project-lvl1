400a7fb28a374106e918dc95587b7d91241a2530
Hanna Mudrak <annmudrak@gmail.com>
2022-06-18 18:03:37 -0500

    add game choice menu

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        String menu = "1 - Greet\n2 - Even\n0 - Exit";
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println(menu);
+        System.out.print("Your choice: ");
+        Scanner getGameNumber = new Scanner(System.in);
+        String gameNumber = getGameNumber.nextLine();
+        switch (gameNumber) {
+            case "1" -> Cli.greeting();
+            case "2" -> Even.isEven();
+            case "0" -> System.out.println("Bye!");
+            default -> System.out.println("There is no such game!");
+        }
     }
 }
