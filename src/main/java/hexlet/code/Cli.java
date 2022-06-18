0a913d48a3f910b8da949e212442f6bfa8d46dad
Hanna Mudrak <annmudrak@gmail.com>
2022-06-18 18:05:18 -0500

    add return statement

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greeting() {
+    public static String greeting() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         String name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
+        return name;
     }
 }
