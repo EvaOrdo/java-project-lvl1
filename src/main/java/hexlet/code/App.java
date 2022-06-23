8e717a49fc4bebebf84f16f1985047b40e914eeb
Hanna Mudrak <annmudrak@gmail.com>
2022-06-23 15:01:27 -0500

    add Prime game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,15 +1,12 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Progression;
+import hexlet.code.games.*;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String menu = "1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit";
+        String menu = "1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit";
         System.out.println("Please enter the game number and press Enter.");
         System.out.println(menu);
         System.out.print("Your choice: ");
@@ -21,6 +18,7 @@ public class App {
             case "3" -> Calc.calc();
             case "4" -> GCD.gcd();
             case "5" -> Progression.progression();
+            case "6" -> Prime.prime();
             case "0" -> System.out.println("Bye!");
             default -> System.out.println("There is no such game!");
         }
---
+++ src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,41 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import hexlet.code.Utils;
+
+public class Prime {
+
+    public static Boolean isPrime(int number) {
+        for (var i = 2; i <= Math.sqrt(number); i += 1) {
+            if (number % i == 0) {
+                return false;
+            }
+        } return true;
+    }
+
+    public static String[] getGameData() {
+        final int MIN_RANGE_NUM = 3;
+        final int MAX_RANGE_NUM = 100;
+        int randomNum = Utils.getRandomNumber(MIN_RANGE_NUM, MAX_RANGE_NUM);
+        String question = "" + randomNum;
+        String answer = isPrime(randomNum) ? "yes" : "no";
+        String[] gameData = {question, answer};
+        return gameData;
+    }
+
+    public static String[][] getGameDataSet(int roundsCount) {
+        String[][] gameDataSet = new String[roundsCount][];
+        for (var i = 0; i < roundsCount; i++) {
+            gameDataSet[i] = getGameData();
+        }
+        return gameDataSet;
+    }
+
+    public static final int ROUNDS_COUNT = 3;
+    public static void prime() {
+        String rule = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String [][] gameData = getGameDataSet(ROUNDS_COUNT);
+
+        Engine.runEngine(rule, gameData);
+    }
+}
