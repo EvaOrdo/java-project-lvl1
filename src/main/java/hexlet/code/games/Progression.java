b701107289a8c554141afb2c1ea277027f608968
Hanna Mudrak <annmudrak@gmail.com>
2022-06-27 15:51:11 -0500

    add buildProgression method

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -11,28 +11,30 @@ public class Progression {
     static final int PROGRESSION_LENGTH = 10;
     static final String RULE = "What number is missing in the progression?";
 
+    public static int[] buildProgression(int startNum, int step) {
+        int[] progression = new int[PROGRESSION_LENGTH];
+        progression[0] = startNum;
+        for (var i = 1; i < PROGRESSION_LENGTH; i++) {
+            progression[i] = progression[i - 1] + step;
+        }
+        return progression;
+    }
     public static String[] getGameRoundData() {
         String[] gameData;
         gameData = new String[2];
-
         int progressionStartNumber = Utils.getRandomNumber(MIN_PROGRESSION_FIRST_NUM, MAX_PROGRESSION_FIRST_NUM);
         int progressionStep = Utils.getRandomNumber(MIN_PROGRESSION_STEP, MAX_PROGRESSION_STEP);
-
         int missingNumberIndex = Utils.getRandomNumber(1, PROGRESSION_LENGTH - 1);
 
-        int[] progression = new int[PROGRESSION_LENGTH];
-
-        progression[0] = progressionStartNumber;
-        String question = "" + progressionStartNumber;
-        String answer = "";
-
-        for (var i = 1; i < PROGRESSION_LENGTH; i++) {
-            progression[i] = progression[i - 1] + progressionStep;
-            if (i == missingNumberIndex) {
+        int[] progression = buildProgression(progressionStartNumber, progressionStep);
+        int missingNum = progression[missingNumberIndex];
+        String question = "";
+        String answer = "" + missingNum;
+        for (var item: progression) {
+            if (item == missingNum) {
                 question += " ..";
-                answer += progression[i];
             } else {
-                question += " " + progression[i];
+                question += " " + item;
             }
         }
 
